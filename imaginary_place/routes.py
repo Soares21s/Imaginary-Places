5d638f23beed90fd9d037b3e7cd04049bdbee99f
SoByte/ <soares21sassa@gmail.com>
2025-10-17 09:24:21 -0300

    write

--- imaginary_place/routes.py
+++ imaginary_place/routes.py
@@ -1,6 +1,13 @@
 from flask import Flask,render_template
 from imaginary_place import app
+from imaginary_place import init_bank
+
+init_bank.create_table()
 
 @app.route('/')
 def homepage():
     return render_template('homepage.html')
+
+@app.route('/write')
+def write():
+    return render_template('write.html')
