8ae2d5a8cd5a4e60ecb29fafacb9abd04c2f01c7
SoByte/ <soares21sassa@gmail.com>
2025-10-17 09:13:00 -0300

    add init_bank

---
+++ imaginary_place/templates/init_bank.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+def connect():
+    conn = sqlite3.connect('bank.db')
+    
+    return conn
+
+def create_table():
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY AUTOINCREMENT,author TEXT NOT NULL, description TEXT NOT NULL, text TEXT NOT NULL)")
+    
+    conn.commit()
+    conn.close() 
